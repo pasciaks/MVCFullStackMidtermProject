e83eb58433acb9534e55e3ec4864cc02587e01af
Sheldon Pasciak <sheldon@pasciak.com>
2024-04-18 14:26:04 -0600

    Stub out boot project; configure gradle

---
+++ RepeatBoot/src/main/java/com/skilldistillery/repeat/RepeatBootApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.repeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatBootApplication.class, args);
+	}
+
+}
---
+++ RepeatBoot/src/test/java/com/skilldistillery/repeat/RepeatBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.repeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
