ec9740a19f68cc0236791c635933da2641507709
Bryan Bradley <bryrbradley@gmail.com>
2024-04-18 15:57:20 -0600

    mapped intitial User.entity columns

--- RepeatJPA/src/main/java/com/skilldistillery/repeat/entities/User.java
+++ RepeatJPA/src/main/java/com/skilldistillery/repeat/entities/User.java
@@ -1,6 +1,33 @@
 package com.skilldistillery.repeat.entities;
 
-//FIXME: Add JPA annotations to this class
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	
+	private String password;
+	
+	@CreationTimestamp
+	@Column (name = "created_at")
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column (name = "updated_at")
+	private LocalDateTime updatedAt;
+	
 }
